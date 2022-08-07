b76a4ce6042b89be5e7e49bb06a09cad804a93f1
Arsenii Lisunov <arsenii.lis@gmail.com>
2022-08-07 12:55:19 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pydoc_renderer',
+    version='0.1',
+    packages=['example', 'doc_renderer'],
+    url='',
+    license='',
+    author='Arsenii Lisunov',
+    author_email='arsenii.lis@gmail.com',
+    description='PyDoc Renderer is a library that simplifies Google document creation by providing an API to insert texts, images, lists and tables'
+)
